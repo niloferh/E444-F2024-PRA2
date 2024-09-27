0ec8c48edb3be9097e21236e481356d7d2023f80
niloferh <nilofer.hyder@mail.utoronto.ca>
2024-09-26 23:10:37 -0400

    (Activity 1.3) Added timestamp in LLLL format

--- hello.py
+++ hello.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 @app.route('/')
 def index():
@@ -10,4 +13,4 @@ def index():
 
 @app.route('/user/<name>')
 def user(name):
-    return render_template('user.html', name=name)
+    return render_template('user.html', name=name, current_time=datetime.utcnow())
